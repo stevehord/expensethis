4a90ae59d8f876b157f8a63a49032cd5f307aab4
STEPHEN HORD <Steve@STEPHENs-MacBook-Pro.local>
2018-02-10 14:27:13 -0800

    Filterpart

--- src/oData/ODataBuilder.ts
+++ src/oData/ODataBuilder.ts
@@ -11,6 +11,24 @@ import Logger from "../utils/Logger";
             return undefined;
         }
 
+        const part: Array<string> = queryString.split("&");
+
+        const query: {[k: string]: any} = {};
+        const queryPart: {[k: string]: any} = {};
+        queryPart[part[1]] = part[2];
+        query[part[0]] = queryPart;
+
+        ODataBuilder.logger.debug(query.toString());
+        return query;
+
+    }
+
+    public static filterPart(queryString: string): any {
+
+        if (queryString == undefined || queryString === "") {
+            return undefined;
+        }
+
         const part: Array<string> = queryString.split(" ");
 
         const query: {[k: string]: any} = {};
